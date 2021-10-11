cd96cb978919ec2b1e998a27b028dd24a43afd93
isaa <isaa@gmail.com>
2021-10-12 01:41:51 +0200

    the project view layout for home, ride files

---
+++ src/views/Home.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+    <!-- form details -->
+      <div class="col-xs-6 col-md-4 divform">
+        <form @submit.prevent="UserDetails">
+          <label>User Detail</label>
+          <input type="text" required v-modal="text" placeholder="Enter User details">
+          <div v-if="iderror" class="error">{{iderror}}</div><br/>
+          <div>
+            <!-- form radio -->
+            <b class="radio-inline" >
+              <input type="radio" name="required" checked >User ID
+            </b>
+            <b class="radio-inline" >
+              <input type="radio" name="required" >Email
+            </b>
+            <b class="radio-inline" >
+              <input type="radio" name="required" >Phone
+            </b>
+            <div v-if="iderror" class="error">{{iderror}}</div>
+            </div>
+            <!-- form button -->
+          <div class="sumbit">
+            <button class="btn btn-info">User Details</button>
+          </div>
+        </form>
+      </div>
+      <!-- form details -->
+      <div class="col-xs-6 col-md-4 divform">
+        <form @submit.prevent="VehicleDetails" >
+          <label>Driver Detail</label>
+          <input type="text" required v-modal="text" placeholder="Enter User details"><br>
+          <div>
+            <!-- form radio -->
+            <b class="radio-inline" >
+              <input type="radio" name="required" checked >Driver ID
+            </b>
+            <b class="radio-inline" >
+              <input type="radio" name="required" >Phone
+            </b>
+            <b class="radio-inline" >
+              <input type="radio" name="required" >Vehicle No
+            </b>
+            <div v-if="iderror" class="error">{{iderror}}</div>
+            </div>
+            <!-- form button -->
+          <div class="sumbit">
+            <button class="btn btn-info">Driver Details</button>
+          </div>
+        </form>
+      </div>
+      <!-- form details -->
+      <div class="col-xs-6 col-md-4" style="text-align: center">
+        <form @submit.prevent="RideDetails" >
+          <label>Ride Detail</label>
+          <input type="text" required v-modal="text" placeholder="Enter User details"><br>
+          <div>
+            <!-- form radio -->
+            <b class="radio-inline" >
+              <input type="radio" name="Ride_ID" checked >Ride ID
+            </b>
+            <div v-if="iderror" class="error">{{iderror}}</div>
+            </div>
+            <!-- form button -->
+          <div class="sumbit">
+            <button class="btn btn-info">Ride Details</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { BCard, BCardText } from 'bootstrap-vue'
+
+export default {
+  components: {
+    // BCard,
+    // BCardText,
+    // BLink,
+  },
+  data() {
+    return {
+      text: '',
+      password: '',
+      clcked: true,
+      iderror: '',
+    }
+  },
+  methods: {
+    UserDetails() {
+
+    },
+    VehicleDetails() {
+
+    },
+    RideDetails() {
+      this.iderror = this.text.length > 0 ? '' : 'id needed'
+    },
+  },
+}
+</script>
+
+<style>
+  .divform {
+    border-right: 1px solid #ddd;
+    text-align: center;
+  }
+  span{
+    color: black;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 1em;
+    letter-spacing:1px;
+    font-weight: bold;
+  }
+  input{
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+    text-align: center;
+  }
+  input[type="radio"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+  }
+  input, select {
+    display:block;
+    padding:10px 6px;
+    width:100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+  }
+  button {
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top:20px;
+    color: white;
+    border-radius: 2px;
+  }
+  submit {
+    text-align: center;
+  }
+  .error {
+    color: red;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
+  }
+</style>
---
+++ src/views/SecondPage.vue
@@ -0,0 +1,152 @@
+<template>
+  <div >
+    <div class=" container-fluid divback">
+       <div>
+         <!-- search by date -->
+            <form @submit.prevent="UserDetails" >
+                <input type="date" required >
+                <div v-if="iderror" class="error">{{iderror}}</div>&nbsp;&nbsp;
+                <button class="btn btn-info">Search</button>
+            </form>
+        </div>
+    </div><br>
+    <div class="container-fluid divback">
+        <div>
+          <!-- search by entries -->
+            <label>Show</label>&nbsp;&nbsp;
+            <!-- select -->
+            <Select class="select">
+                <option>50</option>
+                <option>20</option>
+                <option>10</option>
+            </select>&nbsp;&nbsp;
+            <label>entries</label>
+        <span class="float-right">
+          <!-- search buttons -->
+        <label>Search</label>&nbsp;&nbsp;
+        <input type="search" v-modal="search" >
+        </span>
+        </div>
+          <!-- table -->
+          <table class="table table-responsive table-striped">
+              <tr>
+                <th>EngagementID</th>
+                  <th>DriverName</th>
+                  <th>UserName</th>
+                  <th>UserPhoneNo</th>
+                  <th>PickUp</th>
+                  <th>Drop</th>
+              </tr>
+              <tr><td colspan="6"><strong>No datain table:</strong></td></tr>
+          </table>
+          <!-- pagination -->
+        <div>
+          <span>showing 0 to 0 of 0 entries</span>
+          <p><ul class="pagination justify-content-end">
+              <li class="page-item"><a class="page-link" href="#">First</a></li>
+              <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+              <li class="page-item"><a class="page-link" href="#">Next</a></li>
+              <li class="page-item"><a class="page-link" href="#">Last</a></li>
+          </ul></p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { BCard, BCardText } from 'bootstrap-vue'
+
+export default {
+  components: {
+    // BCard,
+    // BCardText,
+    // BLink,
+  },
+  data() {
+    return {
+      text: '',
+      password: '',
+      clcked: true,
+      iderror: '',
+    }
+  },
+  methods: {
+    UserDetails() {
+
+    },
+    VehicleDetails() {
+
+    },
+    RideDetails() {
+      this.iderror = this.text.length > 0 ? '' : 'id needed'
+    },
+  },
+}
+</script>
+
+<style>
+  .divf {
+    border-radius: 0px;
+    border: 1px solid #ddd;;
+    background:white;
+    margin-bottom: 50px;
+    padding: 0.6em;
+    max-width: 100%;
+  }
+  .form{
+    max-width:320px;
+    margin: 30px auto;
+    text-align: center;
+    padding:40px;
+    border-radius: 10px;
+  }
+  label{
+    color: black;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 1em;
+    text-transform: uppercase;
+    letter-spacing:1px;
+    font-weight: bold;
+  }
+  input[type="date"]{
+    display:inline-block;
+    padding: 10px 6px;
+    width: 350px;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+    text-align: center;
+  }
+  .submit1{
+      width: 400px;
+      display:inline-block;
+  }
+  .select {
+    display:inline-block;
+    padding:10px 6px;
+    width:80px;
+    box-sizing: border-box;
+    border: 1px solid #ddd;
+    color: #555;
+  }
+  input[type="search"] {
+    display:block;
+    padding:10px 6px;
+    width:250px;
+    box-sizing: border-box;
+    border: 1px solid #ddd;
+    color: #555;
+    display:inline-block;
+  }
+  .span{
+      border: 1px solid #ddd;
+      padding: 0.4em;
+  }
+  .table {
+    padding: 0.5em;
+    text-align: center;
+
+  }
+</style>
