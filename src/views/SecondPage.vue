5f990ca333fea25a166a81873231f1f4b639eab9
isaa <isaa@gmail.com>
2021-10-14 17:28:43 +0200

    added more features to vue table

--- src/views/SecondPage.vue
+++ src/views/SecondPage.vue
@@ -26,16 +26,51 @@
         <label>Search</label>&nbsp;&nbsp;
         <b-form-input v-model="filter" type="search" id="filterInput" placeholder=""></b-form-input>
         </span>
-        </div>
+        </div><br><br>
           <!-- table -->
-          <b-table striped hover outlined :items="posts" :filter="filter"></b-table>
+           <b-table
+      :items="items"
+      :fields="fields"
+      :current-page="currentPage"
+      :per-page="perPage"
+      :filter="filter"
+      :filter-included-fields="filterOn"
+      :sort-by.sync="sortBy"
+      :sort-desc.sync="sortDesc"
+      :sort-direction="sortDirection"
+      show-empty
+      small
+      responsive
+      @filtered="onFiltered"
+    >
+      <template #cell(name)="row">
+        {{ row.value.first }} {{ row.value.last }}
+      </template>
+
+      <template #cell(actions)="row">
+        <b-button size="sm" @click="info(row.item, row.index, $event.target)" class="mr-1">
+          Info modal
+        </b-button>
+        <b-button size="sm" @click="row.toggleDetails">
+          {{ row.detailsShowing ? 'Hide' : 'Show' }} Details
+        </b-button>
+      </template>
+
+      <template #row-details="row">
+        <b-card>
+          <ul>
+            <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value }}</li>
+          </ul>
+        </b-card>
+      </template>
+    </b-table>
           <!-- pagination -->
         <div>
-          <span>showing 0 to 0 of 0 entries</span>
+          <span>showing {{ currentPage }} to {{ perPage }} of {{ totalRows }} entries</span>
           <p class="pagination justify-content-end">
             <b-pagination
           v-model="currentPage"
-          :total-rows="rows"
+          :total-rows="totalRows"
           :per-page="perPage"
           aria-controls="my-table"
         ></b-pagination>
@@ -50,31 +85,130 @@ export default {
   name: 'App',
   data() {
     return {
-      perPage: 1,
-      currentPage: 1,
-      filter: '',
-      posts: [
-        {
-          userId: 1,
-          id: 1,
-          title: 'sunt aut facere repellat provident occaecati',
-        },
-        {
-          userId: 1,
-          id: 2,
-          title: 'qui est esse',
-        },
-        {
-          userId: 1,
-          id: 3,
-          title: 'ea molestias quasi exercitationem repellat qui',
-        },
+      // perPage: 1,
+      // currentPage: 1,
+      // filter: '',
+      // sortBy: 'EngagementID',
+      // sortDesc: false,
+      // fields: [
+      //   {
+      //     Key: 'EngagementID',
+      //     sortable: true,
+      //   },
+      // ],
+      // posts: [
+      //   {
+      //     EngagementID: '',
+      //     DriverName: '',
+      //     UserName: '',
+      //     UserPhoneNo: '',
+      //     Pickup: '',
+      //     Drop: '',
+      //     RequestTime: '',
+      //     VehicleType: '',
+      //     Status: '',
+      //     EndRide: '',
+      //   },
+      // ],
+      items: [
+        // { isActive: true, age: 40, name: { first: 'Dickerson', last: 'Macdonald' } },
+        // { isActive: false, age: 21, name: { first: 'Larsen', last: 'Shaw' } },
+        // {
+        //   isActive: false,
+        //   age: 9,
+        //   name: { first: 'Mini', last: 'Navarro' },
+        //   _rowVariant: 'success',
+        // },
+        // { isActive: false, age: 89, name: { first: 'Geneva', last: 'Wilson' } },
+        // { isActive: true, age: 38, name: { first: 'Jami', last: 'Carney' } },
+        // { isActive: false, age: 27, name: { first: 'Essie', last: 'Dunlap' } },
+        // { isActive: true, age: 40, name: { first: 'Thor', last: 'Macdonald' } },
+        // {
+        //   isActive: true,
+        //   age: 87,
+        //   name: { first: 'Larsen', last: 'Shaw' },
+        //   _cellVariants: { age: 'danger', isActive: 'warning' },
+        // },
+        // { isActive: false, age: 26, name: { first: 'Mitzi', last: 'Navarro' } },
+        // { isActive: false, age: 22, name: { first: 'Genevieve', last: 'Wilson' } },
+        // { isActive: true, age: 38, name: { first: 'John', last: 'Carney' } },
+        // { isActive: false, age: 29, name: { first: 'Dick', last: 'Dunlap' } },
       ],
+      fields: [
+        {
+          key: 'EngagementID', label: 'Engagement ID', sortable: true, sortDirection: 'desc',
+        },
+        {
+          key: 'DriverName', label: 'Driver Name', sortable: true, class: 'text-center',
+        },
+        {
+          key: 'UserName', label: 'User Name', sortable: true, class: 'text-center',
+        },
+        {
+          key: 'UserPhoneNo', label: 'User Phone No', sortable: true, class: 'text-center',
+        },
+        {
+          key: 'Pickup', label: 'Pickup', sortable: true, class: 'text-center',
+        },
+        {
+          key: 'Drop', label: 'Drop', sortable: true, class: 'text-center',
+        },
+        {
+          key: 'RequestTime', label: 'Request Time', sortable: true, class: 'text-center',
+        },
+        {
+          key: 'VehicleType', label: 'Vehicle Type', sortable: true, class: 'text-center',
+        },
+        {
+          key: 'Status', label: 'Status', sortable: true, class: 'text-center',
+        },
+        {
+          key: 'EndRide', label: 'End ide', sortable: true, class: 'text-center',
+        },
+        // {
+        //   key: 'isActive',
+        //   label: 'Is Active',
+        //   // formatter: (value, key, item) => { return value ? item : key },
+        //   sortable: true,
+        //   sortByFormatted: true,
+        //   filterByFormatted: true,
+        // },
+        // { key: 'actions', label: 'Actions' },
+      ],
+      totalRows: 1,
+      currentPage: 1,
+      perPage: 5,
+      pageOptions: [5, 10, 15, { value: 100, text: 'Show a lot' }],
+      sortBy: '',
+      sortDesc: false,
+      sortDirection: 'asc',
+      filter: null,
+      filterOn: [],
+      infoModal: {
+        id: 'info-modal',
+        title: '',
+        content: '',
+      },
     }
   },
-  computed: {
-    rows() {
-      return this.posts.length
+  mounted() {
+    // Set the initial number of items
+    this.totalRows = this.items.length
+  },
+  methods: {
+    info(item, index, button) {
+      this.infoModal.title = `Row index: ${index}`
+      this.infoModal.content = JSON.stringify(item, null, 2)
+      this.$root.$emit('bv::show::modal', this.infoModal.id, button)
+    },
+    resetInfoModal() {
+      this.infoModal.title = ''
+      this.infoModal.content = ''
+    },
+    onFiltered(filteredItems) {
+      // Trigger pagination to update the number of buttons/pages due to filtering
+      this.totalRows = filteredItems.length
+      this.currentPage = 1
     },
   },
 }
